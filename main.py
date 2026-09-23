bc32a9baef7ce5bbe1c35a0eae57d961a31d5c1a
Dmytro Pinkovskyi <dimapinko3@gmail.com>
2026-09-23 18:37:49 +0300

    Lab 1: Hello World, variables and operators

---
+++ main.py
@@ -0,0 +1,40 @@
+print("Hello, World!")
+
+name = "Dmytro"
+age = 16
+height = 1.80
+is_student = True
+
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+print(is_student)
+print(type(is_student))
+a = 10
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+print(True and False)
+print(True or False)
+print(not True)
